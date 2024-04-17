54f221dbfe15853c9c076b197c4e3a706e84cc3f
Nathaniel Kemme Nash <nkemme54@gmail.com>
2024-04-17 14:03:57 -0400

    chore: Update paths for project links in WebDesignProjects component

--- src/WebDesign/Projects/projects.tsx
+++ src/WebDesign/Projects/projects.tsx
@@ -19,12 +19,12 @@ function WebDesignProjects() {
         <h1 className="title">Web Design Projects</h1>
         <p className="subtitle"></p>
         <div className='courses'>
-          <CardTileVertical title="Tic Tac Toe" subtitle="Simple Tic-Tac-Toe game" theme="tic" difficulty='Easy' onClick={() => {window.location.href = window.location.href + "tic-tac-toe.html"}}/>
-          <CardTileVertical title="Todo List" subtitle="Simple todo list to learn databases" difficulty='Easy' theme="todo" onClick={() => {window.location.href = window.location.href + "todo-list.html"}}/>
-          <CardTileVertical title="Cookie Clicker" subtitle="Cookie Clicker clone that can range in complexity" difficulty='Medium' theme="cookie" onClick={() => {window.location.href = window.location.href + "cookie-clicker.html"}}/>
-          <CardTileVertical title="Blog" subtitle="Practical website to learn UX and more about databases" difficulty='Medium' theme="blog" onClick={() => {window.location.href = window.location.href + "blog.html"}}/>
-          <CardTileVertical title="Slideshow" subtitle="Display photos from a google drive, learn how to use APIs" difficulty='Hard' theme="slideshow" onClick={() => {window.location.href = window.location.href + "slideshow.html"}}/>
-          <CardTileVertical title="E-Commerce Store" subtitle="Another practical project, opition to work with APIs or databases" difficulty='Hard' theme="store" onClick={() => {window.location.href = window.location.href + "ecommerce.html"}}/>
+          <CardTileVertical title="Tic Tac Toe" subtitle="Simple Tic-Tac-Toe game" theme="tic" difficulty='Easy' onClick={() => {window.location.href = window.location.href + "TicTacToe/"}}/>
+          <CardTileVertical title="Todo List" subtitle="Simple todo list to learn databases" difficulty='Easy' theme="todo" onClick={() => {window.location.href = window.location.href + "ToDo/"}}/>
+          <CardTileVertical title="Cookie Clicker" subtitle="Cookie Clicker clone that can range in complexity" difficulty='Medium' theme="cookie" onClick={() => {window.location.href = window.location.href + "CookieClicker/"}}/>
+          <CardTileVertical title="Blog" subtitle="Practical website to learn UX and more about databases" difficulty='Medium' theme="blog" onClick={() => {window.location.href = window.location.href + "Blog/"}}/>
+          <CardTileVertical title="Slideshow" subtitle="Display photos from a google drive, learn how to use APIs" difficulty='Hard' theme="slideshow" onClick={() => {window.location.href = window.location.href + "Slideshow/"}}/>
+          <CardTileVertical title="E-Commerce Store" subtitle="Another practical project, opition to work with APIs or databases" difficulty='Hard' theme="store" onClick={() => {window.location.href = window.location.href + "Ecommerce/"}}/>
           
         </div>
         <br />
